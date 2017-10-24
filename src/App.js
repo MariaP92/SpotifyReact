5be597b257ef357e556cd31a479adbc25c087c6a
esther <estheralvaro44@gmail.com>
2017-10-24 17:09:11 -0500

    es

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Search from './Search';
 import Home from './Home';
 import Start from './Start'
+import Install from './Install'
 
 import {
 	BrowserRouter,
@@ -23,6 +24,7 @@ const App = (props) => {
 					<li><NavLink to="/search"><a>Buscar</a></NavLink></li>
 					<li><NavLink to="/start"><a>Inicio</a></NavLink></li>
 					<li><NavLink to="/your_music"><a>Tu Música</a></NavLink></li>
+          <li><NavLink to="/install"><a>Inicio</a></NavLink></li>
 				</ul>
 			</nav>
 		
@@ -32,6 +34,7 @@ const App = (props) => {
 				<Route  path="/search" render={() => <Search model={model} />}/>									   
 				<Route  path="/home" render={() => <Home model={model} />}/>
 				<Route  path="/start" render={() => <Start model={model} />}/>
+        <Route  path="/install" render={() => <Install model={model} />}/>
 				<Route component={Home}/>
 			</Switch>
 		</div>
--- src/install.js
+++ src/install.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
-import { Grid, Row, Col } from 'react-bootstrap';
+import './Install.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  NavLink
+} from 'react-router-dom'
 
-const install = () => {
+
+
+const Install = () => {
   return (
-    <Grid>
-      <Row className="show-grid">
-        <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-        <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-      </Row>
-    </Grid>
+    <div className="hw-accelerate">
+      <section style={{ height: '100%' }}>
+        <header className="ZkMs_b0D5CxRkpMsqWenC">
+        </header>
+        <div className="downloadPage">
+          <div className="downloadPage-inner">
+            <img src="/static/images/devices/mac.png" />
+            <h1
+              className="_1ggXflHuWSAAo1nVpFSlQp _1dJ5wNF1VI49YsWnX45E9Y"
+              style={{ textAlign: 'center' }}>
+              ¿Ya conoces nuestra aplicación de escritorio?
+</h1>
+            <h2
+              className="_1ggXflHuWSAAo1nVpFSlQp _3jDzDkO6Of6ut021offe4R"
+              style={{ textAlign: 'center' }}>
+              ¡Descárgala para tener la mejor experiencia Spotify!
+</h2>
+            <a
+              className="btn btn-green"
+              target="_blank"
+              href="https://www.spotify.com/download">
+              DESCARGAR APP
+</a>
+          </div>
+        </div>
+      </section>
+    </div>
+
   );
 }
 
 
-export default install;
+export default Install;
