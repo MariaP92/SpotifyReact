ab0aa5321e62c9406e7a8bed56c0e5948a9f0f00
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-10-24 16:06:00 -0500

    primero-gladys

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import YHome from './y-home.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<YHome/>, document.getElementById('root'));
 registerServiceWorker();
+gi
---
+++ src/y-home.js
@@ -0,0 +1,21 @@
+import './y-home.css';
+import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+
+const YHome = () => {
+        <Grid>
+            <Row className="show-grid y-sidebar">
+                <Col xs={3} md={9}><code>&lt;{'Col xs={3} md={3}'} /&gt;</code>cjhjkdfghdjkfgh</Col>
+                <Col xs={9} md={9}><code>&lt;{'Col xs={9} md={9}'} /&gt;</code>dhkjdhfjkfdhgjkfg</Col>
+            </Row>
+
+            <Row className="show-grid y-footer">
+                <Col xs={12} md={12}><code>&lt;{'Col xs={12} md={12}'} /&gt;</code>khfksdjhfkdsjfhkdsjfyarn start</Col>
+            </Row>
+        </Grid>
+
+}
+export default YHome
+
+
