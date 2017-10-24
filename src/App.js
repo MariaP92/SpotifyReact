294d9fa8b7222954e764d0cf71f86c1265d2a136
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-24 15:54:44 -0700

    Maria

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Home from './Home';
+import Start from './Start'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import {
+	BrowserRouter,
+	Route,
+	Switch,
+	Redirect
+} from 'react-router-dom'
+
+const App = (props) => {
+	const {model} =  props;
+	console.log('app.props', props)
+	return (<BrowserRouter>
+		<div>
+			<Switch>
+				<Route  path="/spotifyreact"
+				       render={() => <Redirect to= {'/home'}/>}/>
+				<Route  path="/home" render={() => <Home model={model} />}/>
+				<Route  path="/start" render={() => <Start model={model} />}/>
+				<Route component={Home}/>
+			</Switch>
+		</div>
+	</BrowserRouter>)
 }
 
 export default App;
