8d345c01a29244aa1f9f53129fb644ec4f536210
DianaQuispe <danaalienmas@gmail.com>
2017-10-24 17:22:38 -0500

    avance en clase

--- src/Start.js
+++ src/Start.js
@@ -5,7 +5,7 @@ import './Start.css';
 class Start extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container ">
                 <div className="row">
                     <div className="col-xs-9 col-xs-offset-3 col-sm-9 col-sm-offset-3 col-md-9 col-md-offset-3 col-lg-9 col-lg-offset-3">
 
