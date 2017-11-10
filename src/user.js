8101e9fbe83538dd26fa0bd8b1cf75c358d60b9f
DianaQuispe <danaalienmas@gmail.com>
2017-11-10 12:30:50 -0500

    sass

--- src/user.js
+++ src/user.js
@@ -1,51 +1,50 @@
 import React, { Component } from "react";
-import "./user.css";
+import "./sass/main.css";
 
 class User extends Component {
   render() {
     return (
-      <div className="container ">
+      <div className="container">
         <div className="row">
-          <div className="col-md-6 col-md-offset-3 text-center">
+          <div className="userCtn col-md-6 col-md-offset-3 text-center">
             <div>
               <img
                 src="http://i0.kym-cdn.com/entries/icons/facebook/000/009/242/yolo.jpg"
                 alt="yolo_imagen"
-                className="img-y accountPage-image"
-              />
+                className="userCtn--img "/>
               <h1>YOLO</h1>
             </div>
-            <div className="button-group button-group--vertical">
-              <div className="button-group__item">
+            <div className="userCtn__btnG">
+              <div>
                 <a
-                  className="btn-y btn-black btn-smal y-a"
+                  className="btn-y  y-a"
                   href="https://www.spotify.com/account/?utm_source=play&utm_campaign=wwwredirect&utm_medium=stp_test"
                   style={{ minWidth: 240 }}
                 >
                   VER CUENTA
                 </a><br/>
               </div>
-              <div className="button-group__item">
+              <div >
                 <a
-                  className="btn-y btn-black btn-smal y-a"
+                  className="btn-y y-a"
                   href="https://www.spotify.com/home?utm_source=play&utm_campaign=wwwredirect&utm_medium=stp_test"
                   style={{ minWidth: 240 }}
                 >
                   SITIO WEB COMPLETO
                 </a>
               </div>
-              <div className="button-group__item">
+              <div>
                 <a
-                  className="btn-y btn-black btn-smal y-a"
+                  className="btn-y y-a"
                   href="https://support.spotify.com/?utm_source=play&utm_campaign=wwwredirect&utm_medium=stp_test"
                   style={{ minWidth: 240 }}
                 >
                   AYUDA
                 </a>
               </div>
-              <div className="button-group__item">
+              <div >
                 <a
-                  className="btn-y btn-black btn-smal y-a"
+                  className="btn-y y-a"
                   href="#"
                   style={{ minWidth: 240 }}
                 >
@@ -55,14 +54,14 @@ class User extends Component {
             </div><br/>
             <div className="footerLinks">
               <a
-                className="footerLinks__item y-a"
+                className="footerLinks__item"
                 href="https://www.spotify.com/legal/"
               >
                 Información legal
               </a>
               <span className="footerLinks__divider">|</span>
               <a
-                className="footerLinks__item y-a"
+                className="footerLinks__item "
                 href="https://www.spotify.com/legal/privacy-policy/"
               >
                 Privacidad
