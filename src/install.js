aca844fd133548b4c6adc6da72fb1e96c24f4bf4
esther <estheralvaro44@gmail.com>
2017-11-10 14:32:21 -0500

    spoty install

--- src/install.js
+++ src/install.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import './Install.css';
+import './sass/main.css';
 
 
 class Install extends Component {
   render() {
     return (
       <div className="row ycontain">
-        <div className="col-xs-2 col-md-2">
+        <div className="col-xs-3 col-md-3">
         </div>
-        <div className="col-md-10 col-lg-10 yinstallcontext">
-          <center><img className="yimgInstall" src="https://i.blogs.es/57d35c/captura-de-pantalla-2016-05-25-a-las-16.11.44/original.png" /></center>
-          < hr/>
+        <div className="col-md-9 col-lg-9 ycontain__ycontext">
+          <center><img className="ycontain__ycontext--yimg" src="https://open.spotify.com/static/images/devices/mac.png" /></center>
           <h1>¿Ya conoces nuestra aplicación de escritorio?</h1>
           <h2>¡Descárgala para tener la mejor experiencia Spotify!</h2>
-          <center><a className="btn btn-green yinstalbtn" target="_blank" href="https://www.spotify.com/download">DESCARGAR APP </a></center>
+          <center><a className="btn btn-green ycontain__ycontext--ybtn" target="_blank" href="https://www.spotify.com/download">DESCARGAR APP </a></center>
         </div>
       </div>
-
     );
   }
 }
