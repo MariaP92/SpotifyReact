95b701f40d04f58401509b6f04dbbe4de3c4c53a
GLADYSYOVANAMAMANICAMATICONA <gladysyovanamamanicamaticona@gmail.com>
2017-11-10 11:49:00 -0500

    gladys

--- src/Home.js
+++ src/Home.js
@@ -36,7 +36,7 @@ class NavWeb extends Component {
 class Home extends Component {
 	render() {
 		return (
-			<div className="container-fluid">
+			<div className="container-fluid ">
 				<div className="row">
 					<div className="col-md-3 col-sm-12 col-xs-12">
 						<NavWeb />
