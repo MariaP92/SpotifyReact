f270cd34fd01f693a2c7193faeb5e306348f0bb7
gabiprds93 <gabiprds.93@gmail.com>
2017-11-10 11:58:21 -0500

    Cambiando a sass

--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import './Search.css';
+import './css/main.css';
 const Search = () => 
 {
     return(
-        <div className="container y-search-container">
+        <div className="container searchContainer">
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h4 className="y-search-h4">Busca un artista, canción, álbum o playlist</h4>
+                    <h4 className="searchContainer__searchTitle">Busca un artista, canción, álbum o playlist</h4>
                 </div>
             </div>
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <input type="text" className="y-search-input" placeholder="Escribir..."/>
+                    <input type="text" className="searchContainer__searchInput" placeholder="Escribir..."/>
                 </div>
             </div>
         </div>
