f082de34b45d2b9333f8404ffee077a72ad70428
esther <estheralvaro44@gmail.com>
2017-11-10 10:37:47 -0500

    instalar aplication

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import Search from './Search';
 import Home from './Home';
 import Start from './Start'
-<<<<<<< HEAD
 import Install from './Install'
-=======
 import User from './user.js'
 import Your_Music from './Your_Music'
->>>>>>> 84bd56d7da96d57454fef51a4e89d9a2fa8455fe
 
 import {
 	BrowserRouter,
@@ -21,31 +18,6 @@ const App = (props) => {
 	const { model } = props;
 	console.log('app.props', props)
 	return (
-<<<<<<< HEAD
-	<BrowserRouter>
-		<div>
-			<nav className='y-web-nav'>
-				<a><img className='y-logo' src='images/logo.png' /></a>
-				<ul className='y-menu'>
-					<li><NavLink to="/search"><a>Buscar</a></NavLink></li>
-					<li><NavLink to="/start"><a>Inicio</a></NavLink></li>
-					<li><NavLink to="/your_music"><a>Tu Música</a></NavLink></li>
-          <li><NavLink to="/install"><a>Inicio</a></NavLink></li>
-				</ul>
-			</nav>
-		
-			<Switch>
-				<Route  path="/spotifyreact"
-				       render={() => <Redirect to= {'/home'}/>}/>
-				<Route  path="/search" render={() => <Search model={model} />}/>									   
-				<Route  path="/home" render={() => <Home model={model} />}/>
-				<Route  path="/start" render={() => <Start model={model} />}/>
-        <Route  path="/install" render={() => <Install model={model} />}/>
-				<Route component={Home}/>
-			</Switch>
-		</div>
-	</BrowserRouter>)
-=======
 		<BrowserRouter>
 			<div>
 				<nav className='y-web-nav'>
@@ -54,6 +26,7 @@ const App = (props) => {
 						<li><NavLink to="/search"><a>Buscar</a></NavLink></li>
 						<li><NavLink to="/start"><a>Inicio</a></NavLink></li>
 						<li><NavLink to="/your_music"><a>Tu Música</a></NavLink></li>
+						<li><NavLink to="/install"><a>Instalar Aplicacion</a></NavLink></li>
 						<li><NavLink to="/user"><a>Yolo</a></NavLink></li>
 
 					</ul>
@@ -67,11 +40,11 @@ const App = (props) => {
 					<Route path="/start" render={() => <Start model={model} />} />
 					<Route path="/user" render={() => <User model={model} />} />
 					<Route path="/your_music" render={() => <Your_Music model={model} />} />
+					<Route path="/install" render={() => <Install model={model} />} />
 					<Route component={Home} />
 				</Switch>
 			</div>
 		</BrowserRouter>)
->>>>>>> 84bd56d7da96d57454fef51a4e89d9a2fa8455fe
 }
 
 export default App;
--- src/install.js
+++ src/install.js
@@ -1,48 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './Install.css';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect,
-  NavLink
-} from 'react-router-dom'
 
 
-
-const Install = () => {
-  return (
-    <div className="hw-accelerate">
-      <section style={{ height: '100%' }}>
-        <header className="ZkMs_b0D5CxRkpMsqWenC">
-        </header>
-        <div className="downloadPage">
-          <div className="downloadPage-inner">
-            <img src="/static/images/devices/mac.png" />
-            <h1
-              className="_1ggXflHuWSAAo1nVpFSlQp _1dJ5wNF1VI49YsWnX45E9Y"
-              style={{ textAlign: 'center' }}>
-              ¿Ya conoces nuestra aplicación de escritorio?
-</h1>
-            <h2
-              className="_1ggXflHuWSAAo1nVpFSlQp _3jDzDkO6Of6ut021offe4R"
-              style={{ textAlign: 'center' }}>
-              ¡Descárgala para tener la mejor experiencia Spotify!
-</h2>
-            <a
-              className="btn btn-green"
-              target="_blank"
-              href="https://www.spotify.com/download">
-              DESCARGAR APP
-</a>
-          </div>
+class Install extends Component {
+  render() {
+    return (
+      <div className="row ycontain">
+        <div className="col-xs-2 col-md-2">
         </div>
-      </section>
-    </div>
+        <div className="col-md-10 col-lg-10 yinstallcontext">
+          <center><img className="yimgInstall" src="https://i.blogs.es/57d35c/captura-de-pantalla-2016-05-25-a-las-16.11.44/original.png" /></center>
+          < hr/>
+          <h1>¿Ya conoces nuestra aplicación de escritorio?</h1>
+          <h2>¡Descárgala para tener la mejor experiencia Spotify!</h2>
+          <center><a className="btn btn-green yinstalbtn" target="_blank" href="https://www.spotify.com/download">DESCARGAR APP </a></center>
+        </div>
+      </div>
 
-  );
+    );
+  }
 }
-
 
 export default Install;
